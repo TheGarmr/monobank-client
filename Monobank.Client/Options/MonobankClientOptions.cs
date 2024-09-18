43dde04754bb1fe18f8c956b1231a5b627c7b660
The Garmr <garmr@honchar.net>
2024-09-18 18:09:47 +0300

    Bugfixes and extension of failure message.

--- Monobank.Client/Options/MonobankClientOptions.cs
+++ Monobank.Client/Options/MonobankClientOptions.cs
@@ -4,13 +4,13 @@ namespace Monobank.Client.Options
 {
     public class MonobankClientOptions
     {
-        internal string ApiBaseUrl { get; set; } = "https://api.monobank.ua/";
-        internal TimeSpan MaxStatementRange => TimeSpan.FromDays(31) + TimeSpan.FromHours(1);
-        internal int HttpClientTimeoutInSeconds { get; set; } = 30;
+        public string ApiBaseUrl { get; set; } = "https://api.monobank.ua/";
+        public TimeSpan MaxStatementRange => TimeSpan.FromDays(31) + TimeSpan.FromHours(1);
+        public int HttpClientTimeoutInSeconds { get; set; } = 30;
 
         /// <summary>
         /// Token for personal access to the API.
         /// </summary>
-        internal string ApiToken { get; set; } = string.Empty;
+        public string ApiToken { get; set; } = string.Empty;
     }
 }
